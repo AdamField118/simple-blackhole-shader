a2681bd0638796387482ae0808660069701215e2
AdamField118 <adfield@wpi.edu>
2024-09-13 09:13:19 -0400

    Notes for meeting, fragment.glsl

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -9,72 +9,102 @@ uniform vec2 uResolution;
 uniform float theta;
 varying vec2 vUv;
 
+//complex number product, (a+bi)+(c+di)=(a+c)+(b+d)i
 vec2 c_p(vec2 x, vec2 y){
     return vec2(x[0]+y[0], x[1]+y[1]);
 }
 
+//complex multiplication, (a+bi)(c+di) ==distribute==> (ac−bd)+(ad+bc)i
 vec2 c_m(vec2 x, vec2 y){
     return vec2(x[0]*y[0] - x[1]*y[1] , x[0]*y[1] + x[1]*y[0]);
 }
 
+//complex division, (a+bi)/(c+di) ==mult by conjugate==> ((a+bi)(c+di))/(c^2+d^2)
 vec2 c_d(vec2 x, vec2 y){
     return c_m(x, vec2(y[0], -y[1]))/(pow(y[0], 2.)+pow(y[1],2.));
 }
 
+//complex exponentiation, complex number (x) to the power of a real number (y)
 vec2 c_pow(vec2 x, float y){
     float mag = pow(sqrt(x[0]*x[0] + x[1]*x[1]), y);
     float angle = y*(atan(x[1],x[0]));
     return vec2(mag*cos(angle), mag*sin(angle));
 }
 
+
 float DRF(float X, float Y, float Z){
 
+    //error tolerance
     float ERRTOL = pow(4.0*D1MACH3, 1.0/6.0);
+
+    //lower and upper bounds for the valid range of inputs X, Y and Z
     float LOLIM  = 5.0 * D1MACH1;
     float UPLIM  = D1MACH2/5.0;
+
+    //see line 88, I think these constants are simply to clean up or simplify the calculation there.
     float C1 = 1.0/24.0;
     float C2 = 3.0/44.0;
     float C3 = 1.0/14.0;
 
     float ans = 0.0;
     if(min(min(X,Y),Z) < 0.0) {
+        //ensures no negative inputs
         return ans;
     }
 
     if(max(max(X,Y),Z) > UPLIM){
+        //ensures no input above the upper limit
         return ans;
     }
 
     if(min(min(X+Y,X+Z),Y+Z) < LOLIM){
+        //ensures no input below the lower limit
         return ans;
     }
 
+    //after feeding through the values and know they're valid, we change their name? Unsure what the N means.
     float XN = X;
     float YN = Y;
     float ZN = Z;
+
+    //used to normalize XN, YN, and ZN
     float MU = 0.;
+
+    //the relative deviation of the respective variable, these deviations indicate how far the variables are from MU
     float XNDEV = 0.;
     float YNDEV = 0.;
     float ZNDEV = 0.;
 
     while(true){
+        //We initialized it above, now we set them!
         MU = (XN+YN+ZN)/3.0;
         XNDEV = 2.0 - (MU+XN)/MU;
         YNDEV = 2.0 - (MU+YN)/MU;
         ZNDEV = 2.0 - (MU+ZN)/MU;
+
+        //finds the greatest deviation value
         float EPSLON = max(max(abs(XNDEV),abs(YNDEV)),abs(ZNDEV));
+        //checks if the greatest deviation is less than the tolerance, then breaks if it is, since the value is accurate enough 
         if(EPSLON < ERRTOL){break;}
+
         float XNROOT = sqrt(XN);
         float YNROOT = sqrt(YN);
         float ZNROOT = sqrt(ZN);
+
+        //symmetric product?
         float LAMDA = XNROOT*(YNROOT+ZNROOT) + YNROOT*ZNROOT;
+
+        //resets the values to an average, getting the values closer to the tolerable values 
         XN = (XN+LAMDA)*0.250;
         YN = (YN+LAMDA)*0.250;
         ZN = (ZN+LAMDA)*0.250;
     }
 
+    //second and third order correction terms?
     float E2 = XNDEV*YNDEV - ZNDEV*ZNDEV;
     float E3 = XNDEV*YNDEV*ZNDEV;
+
+    //series expansion applied to improve the accuracy of the final result
     float S  = 1.0 + (C1*E2-0.10-C2*E3)*E2 + C3*E3;
     ans = S/sqrt(MU);
 
@@ -82,40 +112,59 @@ float DRF(float X, float Y, float Z){
 }
 
 float rawF(float sinphi, float m){
+    //just sin^2(phi)
     float sinphi2 = pow(sinphi, 2.0);
+
+    //uses the previous function to find the value approximation of the elliptical integral times sin(phi), some type of component that varies with the angle?
     float drf = DRF(1. - sinphi2, 1. - m*sinphi2, 1.);
     return sinphi*drf;
 }
 
+// int_{0}^{frac{pi}{2}} frac{d theta}{sqrt{1-m sin^2(theta)}} where y is the 1-m in the integral, the x and z are to help the integral fit the standard form?
 float K(float m){ return DRF(0., 1. - m, 1.);}
 
 float F(float phi, float m){
+    //if phi is within the bounds of rawF, we just use that, if it's outside we go into this function and do something different
     if(abs(phi) > M_PI/2.0){
         // Abramowitz & Stegun (17.4.3)
+
+        //shifts phi to get it to a manageable place
         float phi2 = phi + M_PI/2.0;
+        //normalizes the angle to get the number of complete periods of pi in phi2
         float angle = (phi2/M_PI);
+
+        //lost on this, I think it just combines the results from K and this function
         return 2.0*floor(phi2 / M_PI)*K(m) - rawF(cos((angle - floor(angle))*M_PI), m);
     }
+
+    //first comment in this funct explains this
     return rawF(sin(phi), m);
 }
 
+
+//u is the value you want the amplitude of, m is the elliptic modulus?, tol is the tolerance.
 float am(float u, float m, float tol){
 
     float ambuf[10];
     if(u == 0.){return 0.;}
 
     float sqrt_tol = sqrt(tol);
+
+    //if m is really little (near 0) we use the small angle approximation to get a simple answer
     if(m < sqrt_tol){
         // A&S 16.13.4
         return u - 0.25*m*(u - 0.5*sin(2.0*u));
     }
+
     float m1 = 1. - m;
+    //if m is really big we use an approx that says m is close to 1 which simplifies the function by transforming the argument using hyperbolic functions.
     if(m1 < sqrt_tol){
         // A&S 16.15.4
         float t = tanh(u);
         return asin(t) + 0.25*m1*(t - u*(1. - pow(t, 2.)))*cosh(u);
     }
 
+    //uses an interative method to solve for the amount of times it takes for c to be less than the tolerance
     float a = 1.;
     float b = sqrt(m1);
     float c = sqrt(m);
@@ -132,10 +181,13 @@ float am(float u, float m, float tol){
         n = n + 1;
     }
 
+    //finds the mag of u and returns it
     float phi = a*u*pow(2.,float(n));
     for(int i = n-1; i >= 0; i--){
         phi = 0.5*(phi + asin(ambuf[i]*sin(phi)));
     }
+
+    //the amplitude of the Jacobi elliptic functions
     return phi;
 }
 
